781b9c03a5f118a9f7aa90b11caeea6a4cd1d0ce
Victor Costa ( Desenvolvedor Web ) <70709626+victor9743@users.noreply.github.com>
2022-03-24 17:46:34 -0300

    .

--- projEstacio/resources/views/produtos/indexProduto.blade.php
+++ projEstacio/resources/views/produtos/indexProduto.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('produtos.layout.main')
+@section('title', 'novo produto')
+@section('content')
 
-    <table>
+    <table class="table">
         <thead>
             <tr>
                 <th>produto</th>
@@ -36,5 +30,4 @@
             @endforeach
         </tbody>
     </table>
-</body>
-</html>
+    @endsection
--- projEstacio/resources/views/produtos/newProduto.blade.php
+++ projEstacio/resources/views/produtos/newProduto.blade.php
@@ -4,22 +4,17 @@
 
     <form action="/produtos" method="POST" enctype="multipart/form-data">
         @csrf
-        <input type="hidden" name="id" id="id" value="<?php echo isset($produto) ? $produto->id : ''?>">
-        <input type="text" name="nomeproduto" id="nomeproduto" value="<?php echo isset($produto) ? $produto->nomeproduto : ''?>">
-        <input type="text" name="dtavalidade" id="dtavalidade" value="<?php echo isset($produto) ? $produto->dtavalidade : ''?>">
-        <input type="text" name="codbarras" id="codbarras" value="<?php echo isset($produto) ? $produto->codbarras : ''?>">
-        <input type="text" name="tipoproduto" id="tipoproduto" value="<?php echo isset($produto) ? $produto->tipoproduto : ''?>">
+        <input type="hidden" name="id" id="id" value="<?php echo isset($produto) ? $produto->id : '' ?>">
+        <input type="text" name="nomeproduto" id="nomeproduto" value="<?php echo isset($produto) ? $produto->nomeproduto : '' ?>">
+        <input type="text" name="dtavalidade" id="dtavalidade" value="<?php echo isset($produto) ? $produto->dtavalidade : '' ?>">
+        <input type="text" name="codbarras" id="codbarras" value="<?php echo isset($produto) ? $produto->codbarras : '' ?>">
+        <input type="text" name="tipoproduto" id="tipoproduto" value="<?php echo isset($produto) ? $produto->tipoproduto : '' ?>">
 
         <input type="submit" value="Salvar">
     </form>
-    <div id="dialog" title="Basic dialog">
-        <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the &apos;x&apos; icon.</p>
-    </div>
+  
 @endsection
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
-$( function() {
-    $("#dialog").dialog();
-  } );
+
     
 </script>
